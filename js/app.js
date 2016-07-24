a7657cae92d4d0993de9c40c421309f985a72af0
Alex Weldetsadik <alexweldetsadik@gmail.com>
2016-07-24 14:43:38 -0400

    Non-Interactive Version: Logos hard-coded in HTML doc

---
+++ js/app.js
@@ -0,0 +1,109 @@
+$(function() {
+    $('searchBtn').click(function(e) {
+        e.preventDefault();
+
+    });
+});
+
+function showDefault() {
+
+}
+
+partners = [{
+    Name: 'Partner 1',
+    level: 'kg',
+    program: 'scholarship',
+    fadeLogoSrc: 'images/acm.png',
+    colorLogoSrc: 'images/acm-color.png',
+}, {
+    Name: 'Partner 2',
+    level: 'hs',
+    program: 'loan',
+    fadeLogoSrc: 'images/aci.png',
+    colorLogoSrc: 'images/aci-color.png',
+}, {
+    Name: 'Partner 3',
+    level: 'kg',
+    program: 'none',
+    fadeLogoSrc: 'images/afe.png',
+    colorLogoSrc: 'images/afe-color.png',
+}, {
+    Name: 'Partner 4',
+    level: 'hs',
+    program: 'scholarship',
+    fadeLogoSrc: 'images/aci.png',
+    colorLogoSrc: 'images/aci-color.png',
+}, {
+    Name: 'Partner 5',
+    level: 'kg',
+    program: 'loan',
+    fadeLogoSrc: 'images/afe.png',
+    colorLogoSrc: 'images/afe-color.png',
+}, {
+    Name: 'Partner 6',
+    level: 'hs',
+    program: 'loan',
+    fadeLogoSrc: 'images/aci.png',
+    colorLogoSrc: 'images/aci-color.png',
+}, {
+    Name: 'Partner 7',
+    level: 'kg',
+    program: 'none',
+    fadeLogoSrc: 'images/afe.png',
+    colorLogoSrc: 'images/afe-color.png',
+}, {
+    Name: 'Partner 8',
+    level: 'hs',
+    program: 'scholarship',
+    fadeLogoSrc: 'images/acm.png',
+    colorLogoSrc: 'images/acm-color.png',
+}, {
+    Name: 'Partner 9',
+    level: 'kg',
+    program: 'none',
+    fadeLogoSrc: 'images/acm.png',
+    colorLogoSrc: 'images/acm-color.png',
+}, {
+    Name: 'Partner 10',
+    level: 'hs',
+    program: 'scholarship',
+    fadeLogoSrc: 'images/afe.png',
+    colorLogoSrc: 'images/afe-color.png',
+}, {
+    Name: 'Partner 11',
+    level: 'kg',
+    program: 'scholarship',
+    fadeLogoSrc: 'images/acm.png',
+    colorLogoSrc: 'images/acm-color.png',
+}, {
+    Name: 'Partner 12',
+    level: 'hs',
+    program: 'loan',
+    fadeLogoSrc: 'images/aci.png',
+    colorLogoSrc: 'images/aci-color.png',
+}, {
+    Name: 'Partner 13',
+    level: 'kg',
+    program: 'scholarship',
+    fadeLogoSrc: 'images/aci.png',
+    colorLogoSrc: 'images/aci-color.png',
+}, {
+    Name: 'Partner 14',
+    level: 'hs',
+    program: 'loan',
+    fadeLogoSrc: 'images/afe.png',
+    colorLogoSrc: 'images/afe-color.png',
+}, {
+    Name: 'Partner 15',
+    level: 'kg',
+    program: 'scholarship',
+    fadeLogoSrc: 'images/aci.png',
+    colorLogoSrc: 'images/aci-color.png',
+}, 
+{
+    Name: 'Partner 16',
+    level: 'hs',
+    program: 'none',
+    fadeLogoSrc: 'images/acm.png',
+    colorLogoSrc: 'images/acm-color.png',
+}, ];
